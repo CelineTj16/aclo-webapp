7b6c41a9b9513ddcec96cdef40f5b1d07f987673
Peter Hadi Wijaya <peterhadiwijaya963@gmail.com>
2025-12-10 18:04:43 +0800

    fullstack commit p2

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -6,3 +6,22 @@ export interface User {
   createdAt?: string; // backend currently doesn't return these, but maybe for future improvements
   updatedAt?: string;
 }
+
+// ==== Types for Admin actions =====
+export interface AddUserPayload {
+  name: string;
+  email: string;
+  password: string;
+  role?: "customer" | "admin";
+}
+
+export interface UpdateUserPayload {
+  id: string;
+  name?: string;
+  email?: string;
+  role?: "customer" | "admin";
+}
+
+export interface DeleteUserPayload {
+  id: string;
+}
