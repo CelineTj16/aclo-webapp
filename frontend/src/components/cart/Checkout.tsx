06dd84cf68efef076e49aa8417340ba8f0ddd4f7
jessica2828 <jessica.sylviana.js@gmail.com>
2025-12-31 13:06:45 +0800

    typecheck

--- frontend/src/components/cart/Checkout.tsx
+++ frontend/src/components/cart/Checkout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type FormEvent } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MidtransPayButton from "./MidtransPayButton";
 import ShippingOptionsModal from "./ShippingOptionsModal";
